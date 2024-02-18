bf8cbd9f89ccf0219b9456cbab5c8ce68f2dc040
Bryce McKenney <brycemckenney@gmail.com>
2024-02-17 16:35:06 -0800

    added files

---
+++ components/ChatWidget.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>testing!</h1>
+  </div>
+</template>
+<script>
+export default {
+  setup() {
+    
+  },
+  mounted() {
+    const nuxtApp = useNuxtApp()
+    console.log(nuxtApp.$footy)
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ChatWidget from '../components/ChatWidget.vue'
+</script>
+
+<template>
+  <ChatWidget />
+</template>
